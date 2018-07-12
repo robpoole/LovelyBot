02473edc2f4a0c3845c164325f9f21e787997610
Rob Poole <rp_nyinuk@hotmail.com>
2018-07-12 11:47:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,35 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === 'l! help') {
     	message.reply('pong');
+    	message.author.sendMessage("Hey there! \n \n This is what the LovelyBot currently has up for grabs \n \n !8 [your question] = ask the magic 8-ball a question!")
+  	}
+  	if (message.content.substring(0, 2) === '8!') {
+  		var eightBallResponses = [
+	  		'It is certain', 
+	  		'It is decidedly so', 
+	  		'Without a doubt', 
+	  		'Yes - definitely', 
+	  		'You may rely on it', 
+	  		'As I see it, yes', 
+	  		'Most likely', 
+	  		'Outlook good', 
+	  		'Yes', 
+	  		'Signs point to yes', 
+	  		'Reply hazy, try again', 
+	  		'Ask again later', 
+	  		'Better not tell you now', 
+	  		'Cannot predict now', 
+	  		'Concentrate and ask again', 
+	  		'Don\'t count on it', 
+	  		'My reply is no', 
+	  		'My sources say no', 
+	  		'Outlook not so good', 
+	  		'Very doubtful'
+  		];
+  		var randResponse = eightBallResponses[Math.floor(Math.random() * eightBallResponses.length)];
+  		message.reply(randResponse);
   	}
 });
 
