e303d88f659aba912c7361b48e4a0b1a959ce0e4
Rob Poole <rp_nyinuk@hotmail.com>
2018-07-12 14:48:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ client.on('message', message => {
 		    });
 
 		    var request = require('request'),
-			var cheerio = require('cheerio');
+			    cheerio = require('cheerio');
 
 			request('https://news.ycombinator.com', function (error, response, html) {
 				if (!error && response.statusCode == 200) {
