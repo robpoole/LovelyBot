63ccc5b7219d39256783ed1a02c014df20576656
Rob Poole <rp_nyinuk@hotmail.com>
2018-07-13 11:25:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,10 +38,10 @@ client.on('message', message => {
 
             var embed = message.embeds[0];
 
-            /*var imageUrl = embed.image.url;
+            var imageUrl = embed.image.url;
             var urlCheck = imageUrl.slice(-17);
 
-            let user = client.fetchUser('222047900006481920').then(user => {
+            /*let user = client.fetchUser('222047900006481920').then(user => {
                 user.send(imageUrl+'\n \n');
             });*/
 
